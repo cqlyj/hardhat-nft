ab304de1106b4b5f3b8642921c5db0b98b4423dc
cqlyj <luoyingjie0721@gmail.com>
2024-03-02 17:33:36 +0800

    Add consumer to VRF coordinator mock

--- deploy/02-deploy-random-ipfs-nft.js
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -70,6 +70,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     waitConfirmations: network.config.blockConfirmations || 1,
   });
 
+  await vrfCoordinatorV2Mock.addConsumer(subscriptionId, randomIpfsNft.address);
+
   if (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
